72fef036b4f36d2a897ccea8d5989757c847ed6f
Angus Longmore <anguslongmora@gmail.com>
2024-10-04 11:35:00 +0200

    initial commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace QueueAlert
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
